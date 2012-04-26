53d5ba6ea6722a2b5b11c357c586de9ce2645b14
Ewald Zietsman <ewald@siyavula.org.za>
2012-04-26 15:44:37 +0200

    added NCS Mathematics Grade 12

---
+++ NCS/mathematics/grade-12/build.sh
@@ -0,0 +1,7 @@
+latex --interaction nonstopmode -shell-escape Mathematics_Gr12_Merged.tex 
+latex --interaction nonstopmode -shell-escape Mathematics_Gr12_Merged.tex 
+latex --interaction nonstopmode -shell-escape Mathematics_Gr12_Merged.tex 
+dvipdf Mathematics_Gr12_Merged.dvi
+
+
+
