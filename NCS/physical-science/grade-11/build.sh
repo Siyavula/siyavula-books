6f025f619b86b3572b4d33e97b342d80f8711c7c
Ewald Zietsman <ewald@siyavula.org.za>
2012-04-26 15:20:58 +0200

    added NCS Grade 11 Physical Science

---
+++ NCS/physical-science/grade-11/build.sh
@@ -0,0 +1,4 @@
+latex --interaction nonstopmode -shell-escape PhysicalScience_Gr11.tex
+latex --interaction nonstopmode -shell-escape PhysicalScience_Gr11.tex
+latex --interaction nonstopmode -shell-escape PhysicalScience_Gr11.tex
+dvipdf PhysicalScience_Gr11.dvi
